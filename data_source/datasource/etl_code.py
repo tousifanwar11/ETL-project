45ee694dfdccbc0ffcd3c31eec283e6dc5e2aa3f
Tousif Anwar <tanwar@wgu.edu>
2025-02-05 01:55:38 +0000

    logged first etl

--- data_source/datasource/etl_code.py
+++ data_source/datasource/etl_code.py
@@ -5,3 +5,94 @@ from datetime import datetime
 
 log_file = "log_file.txt"
 target_file = "transformed_data.csv"
+
+#function to extract from csv file
+def extract_from_csv(file_to_process):
+    dataframe = pd.read_csv(file_to_process)
+    return dataframe
+
+#function to extract from JSON file
+def extract_from_json(file_to_process):
+    dataframe = pd.read_json(file_to_process)
+    return dataframe
+
+#function to extract from XML file
+def extract_from_xml(file_to_process):
+    dataframe = pd.DataFrame(columns= ["car_model", "year_of_manufacture", "price", "fuel"])
+    tree = ET.parse(file_to_process)
+    root = tree.getroot()
+    for car in root:
+        car_model = car.find("car_model").text
+        year_of_manufacture = car.find("year_of_manufacture").text
+        price = car.find("price").text
+        fuel = car.find("fuel").text
+        dataframe = pd.concat([dataframe, pd.DataFrame([{"car_model":car_model, "year_of_manufacture":year_of_manufacture, "price":price, "fuel": fuel}])], ignore_index=True)
+    return dataframe
+
+#fucntion to extract
+def extract():
+    extracted_data = pd.DataFrame(columns=["car_model","year_of_manufacture","price","fuel"])
+#create an empty data frame to hold extracted data
+
+    #process all csv files
+    for csvfile in glob.glob("*.csv"):
+        extracted_data = pd.concat([extracted_data, pd.DataFrame(extract_from_csv(csvfile))], ignore_index=True)
+
+    #process all json files
+    for jsonfile in glob.glob("*.json"):
+        extracted_data = pd.concat([extracted_data, pd.DataFrame(extract_from_json(jsonfile))],ignore_index=True)
+
+    #process all xml files
+    for xmlfile in glob.glob("*.xml"):
+        extracted_data = pd.concat([extracted_data, pd.DataFrame(extract_from_xml(xmlfile))],ignore_index=True)
+
+    return extracted_data
+
+#Function for transforming data
+def transform(data):
+    #convert price from USD to Euro and round off the two decimals
+    # 1 USD is 0.96 Euro
+    data['price'] = round(data.price * 0.96,2)
+    
+    return data
+
+#fucntion to load data
+def load_data(target_file, transformed_data):
+    transformed_data.to_csv(target_file)
+
+#logging
+def log_progress(message): 
+    timestamp_format = '%Y-%h-%d-%H:%M:%S' # Year-Monthname-Day-Hour-Minute-Second 
+    now = datetime.now() # get current timestamp 
+    timestamp = now.strftime(timestamp_format) 
+    with open(log_file,"a") as f: 
+        f.write(timestamp + ',' + message + '\n') 
+  
+# Log the initialization of the ETL process 
+log_progress("ETL Job Started") 
+  
+# Log the beginning of the Extraction process 
+log_progress("Extract phase Started") 
+extracted_data = extract() 
+  
+# Log the completion of the Extraction process 
+log_progress("Extract phase Ended") 
+  
+# Log the beginning of the Transformation process 
+log_progress("Transform phase Started") 
+transformed_data = transform(extracted_data) 
+print("Transformed Data") 
+print(transformed_data) 
+  
+# Log the completion of the Transformation process 
+log_progress("Transform phase Ended") 
+  
+# Log the beginning of the Loading process 
+log_progress("Load phase Started") 
+load_data(target_file,transformed_data) 
+  
+# Log the completion of the Loading process 
+log_progress("Load phase Ended") 
+  
+# Log the completion of the ETL process 
+log_progress("ETL Job Ended") 
